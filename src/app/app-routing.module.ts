c50e32c98821d38c6f26e17345d85305460d3fe7
Vadim <vadim.novitskiy.98@gmail.com>
2023-05-24 18:15:30 +0300

    init forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InformationComponent } from './information/information.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InformationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
