88b04fb3429c4779788a766f5119e06069af1814
Vadim <vadim.novitskiy.98@gmail.com>
2023-05-25 12:44:01 +0300

    create breakdown

---
+++ src/app/keywords/keywords.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { KeywordsComponent } from './keywords.component';
+
+
+
+@NgModule({
+  declarations: [
+    KeywordsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class KeywordsModule { }
